210f526d68f06ff9c2d1ebaa74ac0de2366b56c4
Brito <isaac.brito.136@ufrn.edu.br>
2024-09-30 13:13:12 -0300

    semana2.c

--- main.c
+++ main.c
@@ -1,96 +1,210 @@
+//////////////////////////////////////////////////////////////////////////////
+                                ///             Universidade Federal do Rio Grande do Norte                 ///
+                                ///                 Centro de Ensino Superior do Seridó                     ///
+                                ///               Departamento de Computação e Tecnologia                   ///
+                                ///                  Disciplina DCT1106 -- Programação                      ///
+                                ///             Projeto Sistema de Gestão Para Despensa Domestica           ///
+                                ///                Developed by @cailucas_lm & @lelebiglove                 ///
+                                ///////////////////////////////////////////////////////////////////////////////
+                                ///                                Semana 2                                 ///
+                                ///////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
 
 
-void exibirMenu();
-void cadastroPessoa();
-void produtos();
-void orcamento();
-void equipe();
-int main()
+
+int main(void) 
 {
+    void InicioLogin();
+    void exibirMenu(); 
+    void cadastro_Pessoa();
+    void produtos();
+    void controle_estoque();
+    void orcamento();
+    void equipe();
+        
     setlocale(LC_ALL, "Portuguese");
-    printf("Bem vindo ao seu controle de despensas\n");
+    printf("Bem vindo ao seu controle de despesas\n");
     
-    exibirMenu();
+    exibir_menu();
 
     return 0;
 }
 
-void exibirMenu()
+void inicio_login(void)
+{
+    printf("=========================================\n");
+    printf("|               BEM VINDO!              |\n");
+    printf("=========================================\n");
+    printf("|---------------------------------------|\n");
+    printf("|                  LOGIN                |\n");
+    printf("|---------------------------------------|\n");
+    printf("#### 1 - Login                          |\n");
+    printf("#### 2 - Não Possuo Cadastro            |\n");
+    printf("----------------------------------------|\n");
+    printf("#### Selecione uma das opcoes: \n");
+    printf("\n");
+    getchar();
+}
+
+void exibir_menu(void)
 {
     int escolha;
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///   Controle de despensa:                                           ///\n");
-    printf("/// Escolha um módulo:                                                     ///\n");
+    printf("//////////////////////////////////////////////////////////////////////////////\n");
+    printf("///   Escolha um módulo:                                                   ///\n");
     printf("///   1. Cadastro Pessoal                                                  ///\n");
     printf("///   2. Produtos                                                          ///\n");
     printf("///   3. Orçamento                                                         ///\n");
-    printf("///   4. Equipe                                                            ///\n");
+    printf("///   4. controle_estoque                                                  ///\n");
+    printf("///   5. Equipe                                                            ///\n");
     printf("///   0. Sair                                                              ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("//////////////////////////////////////////////////////////////////////////////\n");
+    printf("#### Selecione uma das opcoes: \n");
     scanf("%d", &escolha);
+    printf("\n");
+    getchar();
 }
 
-void cadastroPessoa()
+
+void cadastro_pessoa(void) 
 {
-    char nome[50];
-    char cpf[15];
-    char email[50];
-    char dataNascimento[11];
-
-    printf("Qual o seu nome? ");
-      scanf("%s", nome);
-
-    printf("Digite seu CPF: ");
-      scanf("%s", cpf);
-
-    printf("Qual o seu email? ");
-    
-       scanf("%s", email);
-    printf("Qual a sua data de nascimento? (dd/mm/aaaa) ");
-    
-     scanf("%s", dataNascimento);
-
-    printf("Cadastro realizado com sucesso!\n");
-    printf("Nome: %s\nCPF: %s\nEmail: %s\nData de Nascimento: %s\n", nome, cpf, email, dataNascimento);
+    int escolha;
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///           = = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+    printf("///           = = = = = = = = Cadastro Pessoal  = = = = = = = =             ///\n");
+    printf("///           = = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            ID:                                                          ///\n");
+    printf("///            Nome completo:                                               ///\n");
+    printf("///            CPF:                                                         ///\n");
+    printf("///            Data de Nascimento (dd/mm/aaaa):                             ///\n");
+    printf("///                                                                         ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("#### Selecione uma das opcoes: \n");
+    scanf("%d", &escolha);
+    printf("\n");
+    getchar();
 }
 
-void produtos()
-{ 
-    char marca [50];
-    float  preco;
-    char data [10];
-    
-    printf("Digite a marca do produto: ");;
-    scanf("%s", marca);
-    printf("Digite o preço do produto: ");;
-    scanf("%f", &preco);
-    printf("Digite a data de fabricação");
-    scanf("%s", data);
+void produtos(void)
+{
 
-    printf("Produto cadastrado com sucesso!\n");
-    printf("Marca: %s\nPreço: %.2f\nData de Fabricação: %s\n", marca, preco, data);
+    int escolha;
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///           = = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+    printf("///           = = = = = = = =      Produtos   = = = = = = = = =             ///\n");
+    printf("///           = = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            ID-codigo-de-barra:                                          ///\n");
+    printf("///            Nome:                                                        ///\n");
+    printf("///            Marca:                                                       ///\n");
+    printf("///            Quant_estoque:                                               ///\n");
+    printf("///            Data_validade:                                               ///\n");
+    printf("///            ID-compra:                                                   ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("#### Selecione uma das opcoes: \n");
+    scanf("%d", &escolha);
+    printf("\n");
+    getchar();
 }
-  
-void orcamento()
- {
-    float orcamentoMensal;
-    float orcamentoDia;
-    float orcamentoSemana;
-    printf("Digite o orçamento mensal: ");
-    scanf("%f", &orcamentoMensal);  
-    printf("Digite o orçamento diário: ");
-    scanf("%f", &orcamentoDia); 
-     printf("Digite o orçammento Semanal");
-     scanf("%f", &orcamentoSemana);
- }    
-void equipe()
-{ 
-    printf("Desenvolvedores Caio Lucas e Leandro Isaac");
-    printf("Instagram: caiolucas_lm e lelebiglove");
+
+void estoque(void)
+{
+    int escolha;
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///          = = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
+    printf("///          = = = = = =       Controle_estoque    = = = = = =              ///\n");
+    printf("///          = = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            ID-codigo-de-barra:                                          ///\n");
+    printf("///            ID-compra:                                                   ///\n"); /* sera relacionado ao id codigo de barra */ 
+    printf("///            Data:                                                        ///\n");
+    printf("///            Nome:                                                        ///\n");
+    printf("///            Quantidade:                                                  ///\n");
+    printf("///            preco:                                                       ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("#### Selecione uma das opcoes: \n");
+    scanf("%d", &escolha);
+    printf("\n");
+    getchar();
+}
+
+void orcamento(void)
+{
+    int escolha;
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
+    printf("///            = = = = = = = =   Orçamento   = = = = = = = = =              ///\n");
+    printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            ID-codigo-de-barra:                                          ///\n");
+    printf("///            Data:                                                        ///\n");
+    printf("///            Hora:                                                        ///\n");
+    printf("///            Quantidade:                                                  ///\n");
+    printf("///            Valor gasto:                                                 ///\n");
+    printf("///                                                                         ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    printf("#### Selecione uma das opcoes: \n");
+    scanf("%d", &escolha);
+    getchar();
+}
+
+void equipe(void)
+{
+   
+    int escolha;
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
+    printf("///               Departamento de Computação e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
+    printf("///                  Projeto Sistema de Despensa Domestica                  ///\n");
+    printf("///          Developed by @caiolucas_lm   and @lelebiglove                  ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            Este projeto  foi desenvolvido por:                          ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            Caio Lucas Lopes Medeiros                                    ///\n");
+    printf("///            E-mail: caio.lopes.078@ufrn.edu.br                           ///\n");
+    printf("///            Redes sociais: @caiolucas_lm                                 ///\n");
+    printf("///            Git: https://github.com/BiboDoceiro                          ///\n");
+    printf("///                                                                         ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            Leandro Isaac correia de Brito                               ///\n");
+    printf("///            E-mail:                                                      ///\n");
+    printf("///            Redes sociais:                                               ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    printf("#### Selecione uma das opcoes: \n");
+    scanf("%d", &escolha);
+    getchar();
+
+
+
+}
+
+   
     
-}
     
